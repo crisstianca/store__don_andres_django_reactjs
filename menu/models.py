f1b168c15b479e6672da5633852c676ee2f0ae64
crisstianca <ventura.ca950@gmail.com>
2025-11-04 00:01:36 -0600

    update barcode model

--- menu/models.py
+++ menu/models.py
@@ -19,7 +19,7 @@ class Products(models.Model):
     name = models.CharField(max_length=200, verbose_name="Nombre del Producto")
     category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='abarrote', verbose_name="Categoría")
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Precio')
-    barcode = models.CharField(max_length=50, unique=False, verbose_name='Código de barra', blank=True, null=True )
+    barcode = models.CharField(max_length=50, unique=True, verbose_name='Código de barra', blank=True)
 
     def __str__(self):
         return self.name
